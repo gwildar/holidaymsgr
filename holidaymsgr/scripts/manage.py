c8b1ae11e45ca1a18171b0ef283bc87429f78b8d
Doug Winter <doug.winter@isotoma.com>
2013-11-13 14:18:20 +0000

    stray wingdbstub. may the gods forgive me.

--- holidaymsgr/scripts/manage.py
+++ holidaymsgr/scripts/manage.py
@@ -2,8 +2,6 @@
 import os
 import sys
 
-import wingdbstub
-
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holidaymsgr.settings")
     from django.core.management import execute_from_command_line
