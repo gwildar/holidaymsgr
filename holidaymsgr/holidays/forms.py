c17a3dbb78f664c0fe1a867be1f142d776115d9c
Richard Mitchell <richard.j.mitchell@gmail.com>
2014-02-27 11:46:39 +0000

    Big refactor.

---
+++ holidaymsgr/holidays/forms.py
@@ -0,0 +1,26 @@
+# $Id$
+# encoding: utf-8
+"""holidaymsgr.holidays.forms"""
+__author__ = 'Richard Mitchell <richard.mitchell@isotoma.com>'
+__docformat__ = 'restructuredtext en'
+__version__ = '$Revision$'[11:-2]
+
+from django import forms
+
+
+class MailForm(forms.Form):
+
+    subject = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea)
+    subject.widget.attrs = {'class': 'form-control'}
+    message.widget.attrs = {'class': 'form-control'}
+
+
+class LoginForm(forms.Form):
+
+    email = forms.EmailField(label="Email Address")
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    email.widget.attrs = {'class': 'form-control'}
+    username.widget.attrs = {'class': 'form-control'}
+    password.widget.attrs = {'class': 'form-control'}
--- holidaymsgr/holidays/views.py
+++ holidaymsgr/holidays/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import  render
-from django.http import HttpResponseRedirect
-from django import forms
-from django.conf import settings
+# encoding: utf-8
 
-from . import sieve
+import codecs
+import os.path
 
 from django.forms.util import ErrorList
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import  render
+import pkg_resources
+import sievelib.managesieve
+import sievelib.parser
+
+from holidaymsgr.holidays import forms
+
+
+AWAY_TEMPLATE_PATH = os.path.abspath(
+    pkg_resources.resource_filename(  # @UndefinedVariable
+        'holidaymsgr',
+         os.path.join('holidays', 'sieve', 'away.sieve')
+         )
+    )
+AWAY_TEMPLATE = codecs.open(AWAY_TEMPLATE_PATH, 'r', 'utf8').read()
+SCRIPT_NAME = 'holidaymessage'
+INDEX_TEMPLATE = 'holidays/index.html'
+MESSAGE_TEMPLATE = 'holidays/message.html'
 
 
 class SimpleErrorList(ErrorList):
@@ -17,103 +35,135 @@ class SimpleErrorList(ErrorList):
         if not self:
             return u''
 
-        return u'%s' % ''.join([u'%s' % e for e in self])
+        return u''.join(map(unicode, self))
 
 
-class MailForm(forms.Form):
+def _validate_sieve_credentials(username, password,
+                               client_cls=sievelib.managesieve.Client):
+    sieve_client = client_cls(settings.SIEVE_SERVER_HOST,
+                              settings.SIEVE_SERVER_PORT)
+    try:
+        sieve_client.connect(username, password)
+    except sievelib.managesieve.Error, e:
+        return e
 
-    subject = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
-    subject.widget.attrs = {'class': 'form-control'}
-    message.widget.attrs = {'class': 'form-control'}
-
-
-class LoginForm(forms.Form):
-
-    email = forms.EmailField(label="Email Address")
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    email.widget.attrs = {'class': 'form-control'}
-    username.widget.attrs = {'class': 'form-control'}
-    password.widget.attrs = {'class': 'form-control'}
+    return None
 
 
-def index(request):
-    e = ""
+def index(request, form_cls=forms.LoginForm, render=render,
+          validator=_validate_sieve_credentials):
+    login = form_cls()
+    error = None
     if request.method == 'POST':
-        login = LoginForm(request.POST, error_class=SimpleErrorList)
+        login = form_cls(request.POST, error_class=SimpleErrorList)
         if login.is_valid():  # All validation rules pass
             request.session['email'] = login.cleaned_data['email']
             request.session['username'] = login.cleaned_data['username']
             request.session['password'] = login.cleaned_data['password']
+            request.session['authenticated'] = False
+            error = validator(request.session['username'],
+                              request.session['password'])
+            if error is not None:
+                print error
+            else:
+                request.session['authenticated'] = True
+                return HttpResponseRedirect('message')
 
-            try:
-                s = sieve.sieve("bornite.isotoma.com")
-                s.authenticate(request.session['username'],
-                               request.session['password'])
-            except sieve.SieveError, e:
-                print e
-                return render(request, 'holidays/index.html', {
-                    'login': login,
-                    'error_message': e
-                })
-            request.session['authenticated'] = True
-            return HttpResponseRedirect('message')
-
-    else:
-        login = LoginForm()
-
-    return render(request, 'holidays/index.html', {
+    return render(request, INDEX_TEMPLATE, {
         'login': login,
+        'error_message': error
     })
 
 
-away = """
-require "vacation";
-if allof ( not exists ["list-help", "list-unsubscribe", "list-subscribe", "list-owner", "list-post", "list-archive", "list-id", "Mailing-List"], not header :comparator "i;ascii-casemap" :is "Precedence" ["list", "bulk", "junk"], not header :comparator "i;ascii-casemap" :matches "To" "Multiple recipients of*" ) {{
-vacation :days 14 :addresses ["{email}"] :subject "{line}"
-"{contents}
-";
-redirect "{email}";
-keep;
-}}
-"""
+def _sieve_quote(term):
+    # RFC 5228 §2.4.2.
+    term = term.replace(u'\\', u'\\\\').replace(u'"', u'\\"')
+    return u'"{term}"'.format(term=term)
 
 
-def message(request):
-    try:
-        request.session['authenticated']
-    except Exception:
+def _make_script(email_address, subject, message, handle=SCRIPT_NAME,
+                 template=AWAY_TEMPLATE):
+    return template.format(contents=_sieve_quote(message),
+                           subject=_sieve_quote(subject),
+                           handle=_sieve_quote(handle),
+                           email=_sieve_quote(email_address))
+
+
+def _write_and_activate_script(sieve_client, script,
+                             parser_cls=sievelib.parser.Parser):
+    parser = parser_cls()
+
+    # assert script is valid
+    assert parser.parse(script)
+
+    if not sieve_client.havespace(SCRIPT_NAME, len(script)):
+        raise RuntimeError('No space left on server for new script.')
+    else:
+        if not sieve_client.putscript(SCRIPT_NAME, script):
+            raise RuntimeError('Unable to write script.')
+        if not sieve_client.setactive(SCRIPT_NAME):
+            raise RuntimeError('Unable to activate script.')
+
+
+def _activate(request, sieve_client, form_cls=forms.MailForm,
+              make_script=_make_script,
+              set_script=_write_and_activate_script):
+    mail_form = form_cls(request.POST, error_class=SimpleErrorList)
+    if mail_form.is_valid():
+        message = mail_form.cleaned_data['message']
+        subject = mail_form.cleaned_data['subject']
+        email_address = request.session['email']
+
+        script = make_script(email_address, subject, message)
+        set_script(sieve_client, script)
+
+    return mail_form
+
+
+def _is_holiday_script_active(sieve_client):
+    active_script, _others = sieve_client.listscripts()
+    if active_script == u"ingo":
+        sieve_client.setactive(u'')
+        active_script = u''
+
+    return active_script == SCRIPT_NAME
+
+
+def message(request, form_cls=forms.MailForm,
+            client_cls=sievelib.managesieve.Client,
+            activate=_activate, is_active=_is_holiday_script_active,
+            render=render):
+    if not request.session.get('authenticated', False):
         return HttpResponseRedirect('/')
 
+    mail_form = form_cls()
+    sieve_client = client_cls(settings.SIEVE_SERVER_HOST,
+                              settings.SIEVE_SERVER_PORT)
+    active = False
     username = request.session['username']
     password = request.session['password']
-    email_address = request.session['email']
+    try:
+        sieve_client.connect(username, password)
+    except sievelib.managesieve.Error:
+        request.session['authenticated'] = False
+        del request.session['username']
+        del request.session['password']
+        return HttpResponseRedirect('/')
 
-    s = sieve.sieve(settings.SIEVE_SERVER)
-    s.authenticate(username, password)
-    mail = MailForm()
+    try:
+        if request.method == 'POST':
+            if 'set_away_message' in request.POST:
+                mail_form = activate(request, sieve_client)
+            elif 'deactivate' in request.POST:
+                sieve_client.setactive('')
 
-    # delete the default ingo script if it exists
-    if s.get_active() == "ingo":
-        s.desactivate()
+        active = is_active(sieve_client)
 
-    if request.method == 'POST':
-        if 'set_away_message' in request.POST:
-            mail = MailForm(request.POST, error_class=SimpleErrorList)
-            if mail.is_valid():
-                message = mail.cleaned_data['message']
-                subject = mail.cleaned_data['subject']
-                awaymessage = away.format(contents=message,
-                                          line=subject,
-                                          email=email_address)
-                s.putscript('holidaymessage', awaymessage)
-                s.activate('holidaymessage')
-        elif 'deactivate' in request.POST:
-            if s.get_active:
-                s.desactivate()
+    except sievelib.managesieve.Error:
+        # TODO
+        pass
 
-    return render(request, 'holidays/message.html', {
-        'message_active': s.get_active(),
-        'mail': mail
+    return render(request, MESSAGE_TEMPLATE, {
+        'message_active': active,
+        'mail': mail_form
     })
