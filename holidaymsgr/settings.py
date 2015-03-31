cfd26dc1d704b9f667dd2b5880194023bcdb9ef5
Doug Winter <doug.winter@isotoma.com>
2015-03-31 10:11:58 +0100

    working logging config

--- holidaymsgr/settings.py
+++ holidaymsgr/settings.py
@@ -9,7 +9,7 @@ import os  # @UnusedImport
 ###############################################################################
 ###############################################################################
 
-DEBUG = True
+DEBUG = False
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -62,8 +62,6 @@ WEBSERVER = {
 
 GUNICORN = {
     "USER": "daemon",
-    "ACCESS_LOG": "/path/to/access_log",
-    "ERROR_LOG": "/path/to/error_log",
     "PORT": "8000",
 }
 
@@ -73,35 +71,50 @@ GUNICORN = {
 ###############################################################################
 ###############################################################################
 
+# http://www.caktusgroup.com/blog/2015/01/27/Django-Logging-Configuration-logging_config-default-settings-logger/
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+LOGGING_CONFIG = None
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose':{
+            'format': '[%(asctime)s] [%(name)-15s] [%(levelname)-8s] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
         }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': False,
         },
-    }
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': False,
+        },
+
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
 }
+import logging.config
+logging.config.dictConfig(LOGGING)
+
 
 ###############################################################################
 ###############################################################################
